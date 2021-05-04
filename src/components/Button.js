404914dfcb116f507a859fcc219205cbab5f53fe
patrick-angelos <angelosp07@gmail.com>
2021-05-04 13:22:56 +0300

    Add Display, Button and ButtonPanel Components

---
+++ src/components/Button.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Button = (props) => {
+  const { name } = props;
+  return (
+    <div>{name}</div>
+  );
+};
+
+Button.propTypes = {
+  name: PropTypes.string.isRequired,
+};
+
+export default Button;
