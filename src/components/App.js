ac411be2fa9db85217fa8acd7dd3ac20789ecaf5
patrick-angelos <angelosp07@gmail.com>
2021-05-06 13:44:03 +0300

    Refactor class component to statefull function component using hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,43 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import calculate from '../logic/calculate';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: '',
-      next: '',
-      operation: '',
-    };
-    this.handleClick = this.handleClick.bind(this);
+const App = () => {
+  const [data, setData] = useState({
+    total: '',
+    next: '',
+    operation: '',
+  });
+
+  const handleClick = (button) => {
+    const result = calculate(data, button);
+    setData(result);
+  };
+
+  let { total, next, operation } = data; // eslint-disable-line
+  if (total === '') {
+    total = '0';
   }
 
-  handleClick(button) {
-    const { total, next, operation } = calculate(this.state, button);
-    this.setState({
-      total,
-      next,
-      operation,
-    });
-  }
-
-  render() {
-    const { total, next, operation } = this.state;
-    let result;
-    if (total === '') {
-      result = '0';
-    } else {
-      result = `${total} ${operation} ${next}`;
-    }
-    return (
-      <>
-        <Display result={result} />
-        <ButtonPanel clickHandler={this.handleClick} />
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <Display result={`${total} ${operation} ${next}`} />
+      <ButtonPanel clickHandler={handleClick} />
+    </>
+  );
+};
 
 export default App;
