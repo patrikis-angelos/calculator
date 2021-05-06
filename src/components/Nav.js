3c62c4c6bed86c27172fe9d57ce33fb32ccc764e
patrick-angelos <angelosp07@gmail.com>
2021-05-06 15:56:17 +0300

    Add Navigation links

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import NavLink from './NavLink';
 
 const Nav = () => (
-  <nav>Math Magicians</nav>
+  <nav className="flex nav">
+    <p>Math Magicians</p>
+    <div>
+      <NavLink name="Home" path="/" />
+      <NavLink name="Calculator" path="/" />
+      <NavLink name="Quote" path="/" />
+    </div>
+  </nav>
 );
 
 export default Nav;
